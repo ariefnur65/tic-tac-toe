5257b6e8d231d2c454e27948b6768a351ca1b1d6
axorean65 <ariefnur652010@gmail.com>
2024-02-09 15:18:12 +0700

    Add form input to resize tile

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
 // JavaScript Document
+const defaultScale = 3;
+var scaleTile = defaultScale;
 $(document).ready(function() {
     var x = "x"
     var o = "o"
     var count = 0;
     var o_win = 0;
     var x_win = 0;
+    alert(scaleTile + " x " + scaleTile);
     $('#game li').click(function(){
 
         if ($("#one").hasClass('o') && $("#two").hasClass('o') && $("#three").hasClass('o') || $("#four").hasClass('o') && $("#five").hasClass('o') && $("#six").hasClass('o') || $("#seven").hasClass('o') && $("#eight").hasClass('o') && $("#nine").hasClass('o') || $("#one").hasClass('o') && $("#four").hasClass('o') && $("#seven").hasClass('o') || $("#two").hasClass('o') && $("#five").hasClass('o') && $("#eight").hasClass('o') || $("#three").hasClass('o') && $("#six").hasClass('o') && $("#nine").hasClass('o') || $("#one").hasClass('o') && $("#five").hasClass('o') && $("#nine").hasClass('o') || $("#three").hasClass('o') && $("#five").hasClass('o') && $("#seven").hasClass('o'))
